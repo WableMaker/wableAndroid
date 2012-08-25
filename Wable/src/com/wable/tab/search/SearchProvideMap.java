640dd9d2f22019b5352770a003802b48389a5777
rohjoon <rohjoon@gmail.com>
2012-08-25 15:18:47 +0900

    1. Modify structure of SearchProviderActivity.java 2. Add 3 activity class for searching provider information.

---
+++ Wable/src/com/wable/tab/search/SearchProvideMap.java
@@ -0,0 +1,22 @@
+package com.wable.tab.search;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class SearchProvideMap extends Activity implements OnClickListener{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
