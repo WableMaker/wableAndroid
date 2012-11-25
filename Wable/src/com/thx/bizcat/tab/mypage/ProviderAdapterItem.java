b9cff38e2d92063f322c987ad93a7a9f0e72d83f
Jaekwan <who@are.you>
2012-11-25 20:09:05 +0900

    Add Resource

---
+++ Wable/src/com/thx/bizcat/tab/mypage/ProviderAdapterItem.java
@@ -0,0 +1,71 @@
+package com.thx.bizcat.tab.mypage;
+
+public class ProviderAdapterItem {
+	
+	private String title;
+	private String desc;
+	private String price;
+	private String recommand;
+	private String request;
+	private String status;
+	
+	private int image;
+	private String url;
+	
+	private int type;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getRecommand() {
+		return recommand;
+	}
+	public void setRecommand(String recommand) {
+		this.recommand = recommand;
+	}
+	public String getRequest() {
+		return request;
+	}
+	public void setRequest(String request) {
+		this.request = request;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getImage() {
+		return image;
+	}
+	public void setImage(int image) {
+		this.image = image;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+}
---
+++ Wable/src/com/thx/bizcat/tab/mypage/sub/RequestAskActivity.java
@@ -0,0 +1,64 @@
+package com.thx.bizcat.tab.mypage.sub;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+
+import com.thx.bizcat.R;
+import com.thx.bizcat.http.apiproxy.APICODE;
+import com.thx.bizcat.tab.post.PostActivity;
+import com.thx.bizcat.tab.search.SearchActivity;
+import com.thx.bizcat.tab.setting.SettingActivity;
+import com.thx.bizcat.util.RefHandlerMessage;
+import com.thx.bizcat.util.WeakHandler;
+
+public class RequestAskActivity extends Activity implements OnClickListener, RefHandlerMessage {
+
+	private Context context;
+	private SharedPreferences pref;
+	
+	/* Handler */
+	private WeakHandler mHandler = new WeakHandler(this);
+	public void handleMessage(Message msg) {   
+
+		switch(APICODE.fromInt(msg.what)) {     
+
+		default:
+			break;
+		}
+	}; 
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mybiz_req_ask);
+		context = this;
+		
+		/* 요청목록 */
+		//String lastid = pref.getString("LAST_ID", "");
+		//APIProxyLayer.Instance().RequestMyActiveList(lastid, mHandler);
+				
+		/* 제공목록 */
+		//APIProxyLayer.Instance().ProvideMyActiveList(lastid, mHandler); 
+					
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		
+		
+		switch (v.getId()) {
+		
+
+		}
+	}
+}
