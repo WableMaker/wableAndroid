67139f2881ed2d926658f2f3420eeb9f4f330ce4
videru <videru@gmail.com>
2012-12-01 20:50:36 +0900

    sp_GetProviderDetail_Items

--- Wable/src/com/thx/bizcat/http/apiproxy/JSONParser/sp_GetProviderDetail_Items.java
+++ Wable/src/com/thx/bizcat/http/apiproxy/JSONParser/sp_GetProviderDetail_Items.java
@@ -3,13 +3,31 @@ package com.thx.bizcat.http.apiproxy.JSONParser;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.thx.bizcat.http.apiproxy.JSONParser.Result.sp_GetProviderDetail_Result;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class sp_GetProviderDetail_Items {
-	public List<sp_GetProviderDetail_Result> requestsItem = new ArrayList<sp_GetProviderDetail_Result>();
-	public boolean bsuccess = false;
-	public ResultCode resultCode;
-	public sp_GetProviderDetail_Items() {
+import com.thx.bizcat.http.apiproxy.JSONParser.Result.sp_GetProviderDetail_Result;
+import com.thx.bizcat.http.apiproxy.JSONParser.Result.sp_GetUserInfo_Result;
+import com.thx.bizcat.util.Logger;
+
+public class sp_GetProviderDetail_Items extends sp_Simple_Items {
+	public sp_GetProviderDetail_Result result ;
+	public sp_GetProviderDetail_Items(JSONObject obj) {
+		super(obj);
+		try {
+			
+			if(bsuccess)
+			{
+				result = new sp_GetProviderDetail_Result(obj.getJSONObject("data"));
+			}
+			else//�패�는 errorcode�력
+			{
+				resultCode =ResultCode.valueOf(obj.getString("data"));		
+			}
+			
+		} catch (JSONException e) {
+			Logger.Instance().Write(e);
+		}
 	}
 
 }
