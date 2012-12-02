fdebd94b443af4bb10bb309b3804fadf4b6da91c
videru <videru@gmail.com>
2012-12-02 19:22:48 +0900

    refactoring

--- Wable/src/com/thx/bizcat/http/apiproxy/JSONParser/sp_GetProviderDetail_Items.java
+++ Wable/src/com/thx/bizcat/http/apiproxy/JSONParser/sp_GetProviderDetail_Items.java
@@ -1,13 +1,10 @@
 package com.thx.bizcat.http.apiproxy.JSONParser;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.thx.bizcat.http.apiproxy.JSONParser.Result.sp_GetProviderDetail_Result;
-import com.thx.bizcat.http.apiproxy.JSONParser.Result.sp_GetUserInfo_Result;
 import com.thx.bizcat.util.Logger;
 
 public class sp_GetProviderDetail_Items extends sp_Simple_Items {
