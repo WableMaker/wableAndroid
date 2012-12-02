37ae45ae87bc13b7c11acca9b1638b6032586f0b
hallooo <psu0822@gmail.com>
2012-12-02 12:59:40 +0900

    BiddingOfferAsProvider, BiddingOfferAsRequester 추가 완료

---
+++ Wable/src/com/thx/bizcat/http/apiproxy/JSONParser/Result/sp_BiddingOfferAsProvider_Result.java
@@ -0,0 +1,25 @@
+package com.thx.bizcat.http.apiproxy.JSONParser.Result;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.thx.bizcat.util.Logger;
+
+public class sp_BiddingOfferAsProvider_Result {
+	public String biddingid;
+    public String description;
+    
+	public sp_BiddingOfferAsProvider_Result(JSONObject obj)
+	{
+		try {
+
+			if(!obj.isNull("biddingid"))
+				biddingid= obj.getString("biddingid");
+		
+			if(!obj.isNull("description"))
+				description= obj.getString("description");
+		} catch (JSONException e) {
+			Logger.Instance().Write(e);
+		}
+	}
+}
---
+++ Wable/src/com/thx/bizcat/http/apiproxy/JSONParser/sp_BiddingOfferAsRequester_Items.java
@@ -0,0 +1,39 @@
+package com.thx.bizcat.http.apiproxy.JSONParser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.thx.bizcat.http.apiproxy.JSONParser.Result.sp_BiddingOfferAsRequester_Result;
+import com.thx.bizcat.util.Logger;
+
+public class sp_BiddingOfferAsRequester_Items {
+	public sp_BiddingOfferAsRequester_Result result;
+	public boolean bsuccess = false;
+	public ResultCode resultCode = ResultCode.NONE;
+	
+	public sp_BiddingOfferAsRequester_Items(JSONObject obj) {
+		try {
+			bsuccess = obj.getBoolean("success");
+			
+			if(bsuccess)
+			{
+				result = new sp_BiddingOfferAsRequester_Result(obj.getJSONObject("data"));
+				
+			}
+			else//실패시는 errorcode입력
+			{
+				try
+				{
+					resultCode = ResultCode.valueOf(obj.getString("data"));
+				} catch (JSONException e) {
+					Logger.Instance().Write(e);
+				}
+			}
+			
+		} catch (JSONException e) {
+			Logger.Instance().Write(e);
+		}
+	}
+}
+
+
