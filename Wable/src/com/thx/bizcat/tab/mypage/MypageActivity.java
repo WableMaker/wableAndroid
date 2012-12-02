865980e3ee5719470ca218299c5ae0a8f6d2935b
mac <mac@192.168.0.48>
2012-12-02 14:28:21 +0900

    중간 커밋^^

--- Wable/src/com/thx/bizcat/tab/mypage/MypageActivity.java
+++ Wable/src/com/thx/bizcat/tab/mypage/MypageActivity.java
@@ -18,11 +18,18 @@ import com.thx.bizcat.http.apiproxy.APICODE;
 import com.thx.bizcat.http.apiproxy.APIProxyLayer;
 import com.thx.bizcat.http.apiproxy.JSONParser.sp_GetMyActiveProvides_Items;
 import com.thx.bizcat.http.apiproxy.JSONParser.sp_GetMyActiveRequests_Items;
+import com.thx.bizcat.http.apiproxy.JSONParser.sp_UserGetUpdatedContents_Items;
+import com.thx.bizcat.http.apiproxy.JSONParser.Result.sp_GetMyUpdatedBiddings_Result;
+import com.thx.bizcat.http.apiproxy.JSONParser.Result.sp_GetMyUpdatedMatch_Result;
+import com.thx.bizcat.http.apiproxy.JSONParser.Result.sp_GetMyUpdatedProvides_Result;
+import com.thx.bizcat.http.apiproxy.JSONParser.Result.sp_GetMyUpdatedRequests_Result;
+import com.thx.bizcat.http.apiproxy.JSONParser.Result.sp_GetNewMessage_Result;
 import com.thx.bizcat.tab.mypage.sub.ProviderAskActivity;
 import com.thx.bizcat.tab.mypage.sub.ProviderPostActivity;
 import com.thx.bizcat.tab.mypage.sub.RequestAskActivity;
 import com.thx.bizcat.tab.mypage.sub.RequestPostActivity;
 import com.thx.bizcat.util.RefHandlerMessage;
+import com.thx.bizcat.util.SqlManager;
 import com.thx.bizcat.util.WeakHandler;
 
 public class MypageActivity extends ActivityGroup  implements OnClickListener, RefHandlerMessage {
@@ -67,11 +74,156 @@ public class MypageActivity extends ActivityGroup  implements OnClickListener, R
 		}
 		break;
 
+		case UserGetUpdatedContents:
+		{
+			sp_UserGetUpdatedContents_Items r =(sp_UserGetUpdatedContents_Items) msg.obj;
+			if(r.bsuccess) {
+
+				//r.last_modified_time_bidding
+//				APIProxyLayer.Instance().UserGetUpdatedContents(
+//						RequestRecentSyncTime, 
+//						ProvideRecentSyncTime, 
+//						MatchRecentSyncTime, 
+//						BiddingRecentSyncTime, 
+//						BiddingMessageRecentSyncTime, 
+//						EndBiddingRecentSyncTime, 
+//						EndBiddingMessageRecentSyncTime, 
+//						callback)
+				//listRequest.setAdapter(requestAdapter);
+				
+				LAST_REQUEST = r.last_modified_time_request;
+				LAST_PROVIDE = r.last_modified_time_provide;
+				LAST_MATCH = r.last_modified_time_match;
+				LAST_BIDDING = r.last_modified_time_bidding;
+				LAST_BIDDINGMSG = r.last_modified_time_biddingmessage;
+				
+				//Editor edit =  pref.edit();
+				//edit.putString("LAST_REQUEST", LAST_REQUEST);
+				//edit.putString("LAST_PROVIDE", LAST_PROVIDE);
+				//edit.putString("LAST_MATCH", LAST_MATCH);
+				//edit.putString("LAST_BIDDING", LAST_BIDDING);
+				//edit.putString("LAST_BIDDINGMSG", LAST_BIDDINGMSG);
+				//edit.commit();
+				
+				//SqlManager.Reset(context);
+				
+				for(sp_GetMyUpdatedRequests_Result item : r.newrequests) {
+					
+					String sql = String.format("REPLACE INTO request (_id, user_id,title,description,price,category_id,due_date,lat,lon" +
+							",totwitter,tofacebook,status,receive_recommand,created_time,deleted,modified_time) values " +
+							"(%d,%d, '%s','%s', %d,%d, '%s',%f,%f,%d,%d,%d,%d,'%s',%d,'%s')"
+							, item.id, item.user_id, item.title, item.description, item.price, item.category_id, item.due_date
+							,item.lat, item.lon, item.totwitter ?1:0, item.tofacebook?1:0, item.status, item.receive_recommend?1:0, item.created_time
+							,item.deleted?1:0, item.modified_time);
+					
+					SqlManager.excuteSql(context, sql);
+					
+				}
+				
+				
+				for(sp_GetMyUpdatedProvides_Result item : r.newprovides) {
+					
+					String sql = String.format("REPLACE INTO provide values " +
+							"(%d,%d, '%s',%d, %f,%f, %d,%d, '%s','%s','%s','%s','%s','%s','%s',%d,'%s',%d,%d)"
+							, item.id, item.user_id, item.title, item.min_price, item.lat, item.lon, item.radius,
+							item.status, item.created_time, item.description, item.photo1, item.photo2,item.photo3,
+							item.photo4,item.photo5, item.deleted?1:0,item.modified_time,item.totwitter?1:0,item.tofacebook?1:0);
+					
+					SqlManager.excuteSql(context, sql);
+					
+				}
+				
+				
+				for(sp_GetMyUpdatedMatch_Result item : r.newmatches) {
+					
+					String sql = String.format("REPLACE INTO match values " +
+							"(%d,%d, '%s',%d,%d,%d, '%s','%s',%d, '%s','%s',%d,'%s')"
+							, item.request_id, item.provide_id, item.matched_time, item.status, item.recommend?1:0,item.other_user_id,
+							  item.other_title, item.other_description,item.other_price,item.other_user_photo, item.other_user_name,
+							  item.deleted?1:0,item.modified_time);
+					
+					SqlManager.excuteSql(context, sql);
+					
+				}
+				
+//				public long requester_id;
+//			    public long provider_id;
+//			    
+//			    public long bidding_id;
+//			    
+//			    public Long request_id;
+//			    public Long provide_id;
+//			    
+//			    public int request_price;
+//			    public int provide_price;
+//			    public String created_time;
+//			    public Integer settled_price;
+//			    public int status;
+//			    
+//			    public Boolean requesteraccept;
+//			    public Boolean provideraccept;
+//			    public boolean requesterdelete;
+//			    public boolean providerdelete;
+//			    
+//			    public String approved_time;
+//			    public String completed_time;
+//			    public String requesteraccept_time;
+//			    public String provideraccept_time;
+//			    public String modified_time;
+//			    public String other_user_name;
+//			    public String other_title;
+//			    public String other_description;
+//			    
+//			    public Integer other_price;
+//			    public String other_user_photo;
+//			    public Integer provide_status;
+//			    public Boolean provide_deleted;
+//			    public Integer request_status;
+//			    public Boolean request_deleted;
+				
+				for(sp_GetMyUpdatedBiddings_Result item : r.newbiddings) {
+					
+					String sql = String.format("REPLACE INTO match values " +
+							"(%d,%d,%d,%d,%d, %d,%d,'%s',%d,%d, %d,%d,%d,%d, '%s','%s','%s','%s','%s','%s','%s','%s',%d,'%s',%d,%d,%d,%d)"
+							, "");
+					
+					SqlManager.excuteSql(context, sql);
+					
+				}
+				
+				for(sp_GetNewMessage_Result item : r.newbiddingmessages) {
+					
+					String sql = String.format("REPLACE INTO match values " +
+							"(%d,%d, '%s',%d,%d,%d, '%s','%s',%d, '%s','%s',%d,'%s')"
+							, "");
+					
+					SqlManager.excuteSql(context, sql);
+					
+				}
+
+
+				
+				
+				
+				
+				
+			}
+		}
+			
+			
+			break;
 		default:
 			break;
 		}
 	}; 
 	
+	
+	String LAST_REQUEST = ""; 
+	String LAST_PROVIDE = "";
+	String LAST_MATCH = "";
+	String LAST_BIDDING = "";
+	String LAST_BIDDINGMSG = "";
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -93,8 +245,17 @@ public class MypageActivity extends ActivityGroup  implements OnClickListener, R
 		views[3] = getLocalActivityManager().startActivity("PROV_ASK", new Intent(this, ProviderAskActivity.class)).getDecorView();
 		originView = container;
 		
-		//APIProxyLayer.Instance().RequestMyActiveList("", mHandler);
-		//APIProxyLayer.Instance().ProvideMyActiveList("", mHandler);
+		//LAST_REQUEST = pref.getString("LAST_REQUEST", "");
+		LAST_PROVIDE = pref.getString("LAST_PROVIDE", "");
+		LAST_MATCH = pref.getString("LAST_MATCH", "");
+		LAST_BIDDING = pref.getString("LAST_BIDDING", "");
+		LAST_BIDDINGMSG = pref.getString("LAST_BIDDINGMSG", "");
+
+		
+		//APIProxyLayer.Instance().UserGetUpdatedContents(
+		//		LAST_REQUEST, LAST_PROVIDE, LAST_MATCH, LAST_BIDDING, LAST_BIDDINGMSG, "", "", mHandler);
+		
+
 		
 		
 		
--- Wable/src/com/thx/bizcat/util/SqlManager.java
+++ Wable/src/com/thx/bizcat/util/SqlManager.java
@@ -39,6 +39,11 @@ public class SqlManager {
 		Release();
 	}
 	
+	public static void Reset(Context context) {
+		if(help == null) help = new SqlHelper(context, DB_NAME, VERSION);		
+	    help.Reset(help.getWritableDatabase());	
+	}
+	
 
 }
 
@@ -52,27 +57,41 @@ class SqlHelper extends SQLiteOpenHelper {
 	
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		/* 테이블 생성자 */ 
-
-		// Profile 테이블 
-		db.execSQL("CREATE TABLE profile "
-				+"( idx INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, nation TEXT, code INTEGER, num TEXT);");
+		
+		/* 테이블 생성자 */ 		
+		//Sqlite DataType v3.0 INTEGER, TEXT,	NONE, REAL,	NUMERIC
 		
 		// Request Table
 		db.execSQL("CREATE TABLE request "		   
-				+"( idx INTEGER PRIMARY KEY AUTOINCREMENT, call DATETIME, type INTEGER, code INTEGER, num TEXT, name TEXT);");
+				+"(_id INTEGER PRIMARY KEY, user_id INTEGER, title TEXT, description TEXT, price INTEGER, category_id INTEGER, due_date TEXT," 
+				+"lat INTEGER, lon INTEGER, totwitter INTEGER, tofacebook INTEGER, status INTEGER, receive_recommand INTEGER," 
+				+"created_time TEXT, deleted INTEGER, modified_time TEXT);");
 		
 		// Provider Table
-		db.execSQL("CREATE TABLE provider "
-				+"( idx INTEGER PRIMARY KEY AUTOINCREMENT, type INTEGER, duration INTEGER);");
+		db.execSQL("CREATE TABLE provide "
+				+"( _id INTEGER PRIMARY KEY, user_id INTEGER, title TEXT, min_price INTEGER, lat INTEGER, lon INTEGER, radious INTEGER" +
+				",status INTEGER, created_time TEXT, descrition TEXT, photo1 TEXT, photo2 TEXT, photo3 TEXT, photo4 TEXT, photo5 TEXT," +
+				"deleted INTEGER, modified_time TEXT, totwitter INTEGER, tofacebook INTEGER);");
 		
-		// Category Table
-		db.execSQL("CREATE TABLE category "
-				+"( idx INTEGER PRIMARY KEY AUTOINCREMENT, type INTEGER, duration INTEGER);");
+		// Match Table
+		db.execSQL("CREATE TABLE match "
+				+"( request_id INTEGER PRIMARY KEY, provide_id INTEGER PRIMARY KEY, matched_time TEXT, status INTEGER," +
+				"recommend INTEGER, other_user_id INTEGER, other_title TEXT, other_description TEXT, other_price INTEGER," +
+				"other_user_photo TEXT, other_user_name TEXT, deleted INTEGER, modified_time TEXT);");
+		
+		// Bidding Table 
+		db.execSQL("CREATE TABLE bidding "
+				+"( bidding_id INTEGER PRIMARY KEY, requester_id INTEGER, provider_id INTEGER, request_id INTEGER, provide_id INTEGER," +
+				"request_price INTEGER, provide_price INTEGER, created_time TEXT, settled_price INTEGER, status INTEGER," +
+				"requesteraccept INTEGER, provideraccept INTEGER, requesterdelete INTEGER, providerdelete INTEGER," +
+				"approved_time TEXT, completed_time TEXT, requesteraccept_time TEXT, provideraccept_time TEXT, modified_time TEXT," +
+				"other_user_name TEXT, other_title TEXT, other_description TEXT, other_price INTEGER, other_user_photo TEXT, " +
+				"provide_status INTEGER, provide_deleted INTEGER, request_status INTEGER, request_deleted INTEGER);");
+		
 		
 		// Chat Table
-		db.execSQL("CREATE TABLE chat "
-				+"( idx INTEGER PRIMARY KEY AUTOINCREMENT, type INTEGER, duration INTEGER);");
+		//db.execSQL("CREATE TABLE chat "
+		//		+"( idx INTEGER PRIMARY KEY AUTOINCREMENT, type INTEGER, duration INTEGER);");
 		
 	}
 
@@ -86,6 +105,12 @@ class SqlHelper extends SQLiteOpenHelper {
 		//db.execSQL("DROP TABLE IF EXISTS status");
 		onCreate(db);
 		
+	}
+	
+	public void Reset(SQLiteDatabase db) {
+		db.execSQL("DROP TABLE IF EXISTS request");
+		db.execSQL("DROP TABLE IF EXISTS provide");
+		onCreate(db);
 	}
 	
 	
