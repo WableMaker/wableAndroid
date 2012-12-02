3cdd205b8203fafb94ffd51df1f8cf8eb6cd169f
videru <videru@gmail.com>
2012-12-02 19:38:01 +0900

    ProvideListbyDistance
    ProvideListbyArea

--- Wable/src/com/thx/bizcat/http/apiproxy/IAPIProxyLayer.java
+++ Wable/src/com/thx/bizcat/http/apiproxy/IAPIProxyLayer.java
@@ -1,6 +1,9 @@
 package com.thx.bizcat.http.apiproxy;
 
 import java.util.Date;
+import java.util.List;
+
+import com.thx.bizcat.http.apiproxy.JSONParser.Result.sp_GetProvidesByArea_Result;
 
 import android.os.Handler;
 import android.text.format.Time;
@@ -264,7 +267,45 @@ public interface IAPIProxyLayer {
 	boolean ProvideMyActiveList(String lastid,Handler callback);//Ȱ��ȭ�� �Ǵ� �����������
 	boolean ProvideMyDoneList(String lastid,Handler callback);//�Ϸ��������
 	boolean ProvideListbyTime(String lastid,String keyword,Handler callback);//������
+	
+	//반환 클래스 class sp_GetProvidesByArea_Items
+	//호출 오류시
+	//null
+	//
+	//성공시
+	//public List<sp_GetProvidesByArea_Result> result : 정보
+	//public boolean bsuccess =true;
+    //public ResultCode resultCode = ResultCode .NONE;
+	//
+	//실패시
+	//public List<sp_GetProvidesByArea_Result> result = null
+	//public boolean bsuccess =false;
+    //public ResultCode resultCode = 실패사유;
+	//
+	//서버호출 실패시
+	//public List<sp_GetProvidesByArea_Result> result = null
+	//public boolean bsuccess =false;
+    //public ResultCode resultCode = ResultCode .NONE;		
 	boolean ProvideListbyArea(double north,double south,double ease,double west,String keyword,Handler callback);//������
+	
+	//반환 클래스 class sp_GetProvidesByDistance_Items
+	//호출 오류시
+	//null
+	//
+	//성공시
+	//public List<sp_GetProvidesByDistance_Items> result : 정보
+	//public boolean bsuccess =true;
+    //public ResultCode resultCode = ResultCode .NONE;
+	//
+	//실패시
+	//public List<sp_GetProvidesByDistance_Items> result = null
+	//public boolean bsuccess =false;
+    //public ResultCode resultCode = 실패사유;
+	//
+	//서버호출 실패시
+	//public List<sp_GetProvidesByDistance_Items> result = null
+	//public boolean bsuccess =false;
+    //public ResultCode resultCode = ResultCode .NONE;		
 	boolean ProvideListbyDistance(double lat, double lon,double mindistance,String keyword,Handler callback);//������
 
 	boolean ProvideAdd(String title,String description, int minprice,int category
--- Wable/src/com/thx/bizcat/http/apiproxy/JSONParser/sp_GetProvidesByDistance_Items.java
+++ Wable/src/com/thx/bizcat/http/apiproxy/JSONParser/sp_GetProvidesByDistance_Items.java
@@ -3,13 +3,38 @@ package com.thx.bizcat.http.apiproxy.JSONParser;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.thx.bizcat.http.apiproxy.JSONParser.Result.sp_GetProvidesByDistance_Result;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class sp_GetProvidesByDistance_Items {
-	public List<sp_GetProvidesByDistance_Result> requestsItem = new ArrayList<sp_GetProvidesByDistance_Result>();
-	public boolean bsuccess = false;
-	public ResultCode resultCode;
-	public sp_GetProvidesByDistance_Items() {
+import com.thx.bizcat.http.apiproxy.JSONParser.Result.sp_GetProvidesByDistance_Result;
+import com.thx.bizcat.util.Logger;
+
+public class sp_GetProvidesByDistance_Items extends sp_Simple_Items {
+	public List<sp_GetProvidesByDistance_Result> result ;
+	public sp_GetProvidesByDistance_Items(JSONObject obj) {
+		super(obj);
+		try {
+			
+			if(bsuccess)
+			{
+				// 새로운 요청 목록 처리
+				
+				result = new ArrayList<sp_GetProvidesByDistance_Result>();
+				JSONArray array = obj.getJSONArray("data");
+				for(int i=0;i<array.length();i++)
+				{
+					result.add(new sp_GetProvidesByDistance_Result(array.getJSONObject(i)));
+				}
+			}
+			else//�패�는 errorcode�력
+			{
+				resultCode =ResultCode.valueOf(obj.getString("data"));		
+			}
+			
+		} catch (JSONException e) {
+			Logger.Instance().Write(e);
+		}
 	}
 
 }
